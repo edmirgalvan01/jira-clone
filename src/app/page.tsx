6f0cc3918c7b4fa81ac09f9c612cfc0303d6abc8
Edmir Galvan <edmirgalvaz@gmail.com>
2025-03-03 06:23:18 -0600

    Add Auth layouts and pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-
 export default function Home() {
   return (
     <div className="flex gap-4">
-      <Button size="xs">Primary</Button>
-      <Input placeholder="Example" />
-      <Button variant="secondary">Secondary</Button>
-      <Button variant="destructive">Destructive</Button>
-      <Button variant="ghost">Ghost</Button>
-      <Button variant="muted">Muted</Button>
-      <Button variant="outline">Outline</Button>
-      <Button variant="teritary">Teritary</Button>
+      <h1>Hello World!</h1>
     </div>
   );
 }
